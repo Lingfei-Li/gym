00d5929611f92bb3b8bf995d3a0a0dac091c7c9a
sniu22 <sniu1112@gmail.com>
2017-05-01 16:29:02 -0500

    Random Player

---
+++ sniu/Pong_Rand.py
@@ -0,0 +1,25 @@
+import numpy as np
+import gym
+
+
+env = gym.make('Pong-v0')
+# Set learning parameters
+num_episodes = 1000
+
+epi_r = []
+
+
+
+for i in range(num_episodes):
+    env.reset()
+    done = False
+    one_r = []
+    while not done:
+        action = env.action_space.sample()
+        new_s, reward, done, _ = env.step(action)
+        one_r.append(reward)
+
+    print sum(one_r)
+    epi_r.append(sum(one_r))
+
+
